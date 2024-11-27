60c81936522a2775833cdc6429a58b19b97553b1
Karthik-MP <karthikmp521@gmail.com>
2024-11-27 12:54:46 -0500

    code fixes

--- search/search.pl
+++ search/search.pl
@@ -15,36 +15,27 @@ open_passage(CurrentRoom, NextRoom, Keys) :-
     (locked_door(CurrentRoom, NextRoom, LockType) ; locked_door(NextRoom, CurrentRoom, LockType)),
     member(LockType, Keys).
 
-collect_keys(CurrentRoom, Keys, UpdatedKeys) :-
-    key(CurrentRoom, KeyType),
-    \+ member(KeyType, Keys),
-    !, % Cut to prevent backtracking once the key is collected
-    UpdatedKeys = [KeyType | Keys].
+    collect_keys(CurrentRoom, Keys, UpdatedKeys) :-
+        key(CurrentRoom, KeyType),
+        \+ member(KeyType, Keys),
+        !, % Cut to prevent backtracking once the key is collected
+        UpdatedKeys = [KeyType | Keys].
+    
+    collect_keys(_, Keys, Keys).
 
-collect_keys(_, Keys, Keys).
-
-% Base case: If the current room contains the treasure, return the route
 bfs([[Route, CurrentRoom, _] | _], Route) :- 
-    treasure(CurrentRoom).
+    treasure(CurrentRoom). 
 
-% Recursive case: If no treasure yet, explore adjacent rooms
 bfs([[Route, CurrentRoom, Keys] | Queue], FinalRoute) :-
-    % Explore all possible next routes
     findall(
         [NewRoute, AdjacentRoom, NewKeys],
         (
-            open_passage(CurrentRoom, AdjacentRoom, Keys),  % Check if the passage is open
-            \+ member(move(CurrentRoom, AdjacentRoom), Route),  % Prevent revisiting the same room
-            collect_keys(AdjacentRoom, Keys, NewKeys),  % Collect keys in the adjacent room
-            append(Route, [move(CurrentRoom, AdjacentRoom)], NewRoute)  % Build the new route
+            open_passage(CurrentRoom, AdjacentRoom, Keys), 
+            \+ member(move(CurrentRoom, AdjacentRoom), Route),
+            collect_keys(AdjacentRoom, Keys, NewKeys), 
+            append(Route, [move(CurrentRoom, AdjacentRoom)], NewRoute)
         ),
         PossibleRoutes
     ),
-    % Add the newly discovered routes to the queue
-    (PossibleRoutes \= [] ->
-        append(Queue, PossibleRoutes, UpdatedQueue),
-        bfs(UpdatedQueue, FinalRoute)  % Continue the search with updated queue
-    ;
-        % If no new routes, backtrack or terminate the search (depending on logic)
-        FinalRoute = Route
-    ).
+    append(Queue, PossibleRoutes, UpdatedQueue),
+    bfs(UpdatedQueue, FinalRoute). 
