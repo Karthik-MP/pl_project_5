75517946fd4b0b419e359bbb00e8385d56cca5ae
Karthik-MP <karthikmp521@gmail.com>
2024-11-27 12:40:44 -0500

    code changes

--- parsing/parse.pl
+++ parsing/parse.pl
@@ -1,9 +1,45 @@
+% Main entry point for parsing, assuming X is the list of tokens.
+parse(X) :- lines(X, []).
 
-%%%%%%%%%%%%%%%%%
-% Your code here:
-%%%%%%%%%%%%%%%%%
+lines(X, Z) :-
+    line(X, Y),          % Parse a Line
+    (   rest_lines(Y, Z)  % If semicolon follows, parse more lines
+    ;   Z = Y            % Otherwise, end parsing here (no semicolon)
+    ).
 
-parse(X) :- ????
+lines(X, Z) :- 
+    line(X, Z).       % If no semicolon, just parse one Line.
+
+% Rest of the lines after the first one, separated by ';'
+rest_lines([';' | W], Z) :- 
+    lines(W, Z).  % Continue parsing the next lines after semicolon.
+
+line(X, Z) :-
+    num(X, Y),  % Parse a Num
+    line_continuation(Y, Z). % Parse continuation of the line.
+
+line(X, Z) :- 
+    num(X, Z).   % Base case: just a Num (no comma).
+
+% Continuation of a Line, which can be a comma and another Line.
+line_continuation([',' | W], Z) :- 
+    line(W, Z).   % After a comma, expect another Line.
+
+line_continuation(W, W).  % No continuation, base case.
+
+num(X, Z) :-
+    digit(X, Y),     % Parse a single digit.
+    num_tail(Y, Z).  % Parse the rest of the number (if any).
+
+num_tail([D | T], Z) :- 
+    digit([D | T], Y),  % If another digit follows.
+    num_tail(Y, Z).      % Continue parsing the rest of the number.
+
+num_tail(Y, Y).   % Base case: no more digits.
+
+digit([D | T], T) :-
+    member(D, ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']).  % Check if the character is a digit.
+
 
 % Example execution:
 % ?- parse(['3', '2', ',', '0', ';', '1', ',', '5', '6', '7', ';', '2']).
--- search/search.pl
+++ search/search.pl
@@ -2,4 +2,38 @@
 % Your code goes here:
 %%%%%%%%%%%%%%%%%%%%%%
 
-search(Actions) :- ???
+search(Moves) :-
+    initial(InitialRoom), 
+    bfs([[[ ], InitialRoom, []]], Moves). 
+
+open_passage(CurrentRoom, NextRoom, _) :-
+    door(CurrentRoom, NextRoom);
+    door(NextRoom, CurrentRoom).
+
+open_passage(CurrentRoom, NextRoom, Keys) :-
+    (locked_door(CurrentRoom, NextRoom, LockType);
+    locked_door(NextRoom, CurrentRoom, LockType)),
+    member(LockType, Keys). 
+
+collect_keys(CurrentRoom, Keys, UpdatedKeys) :-
+    (key(CurrentRoom, KeyType), \+ member(KeyType, Keys) ->
+        UpdatedKeys = [KeyType | Keys];
+        UpdatedKeys = Keys).
+
+bfs([[Route, CurrentRoom, _] | _], Route) :- 
+    treasure(CurrentRoom). 
+
+bfs([[Route, CurrentRoom, Keys] | Queue], FinalRoute) :-
+    findall(
+        [NewRoute, AdjacentRoom, NewKeys],
+        (
+            open_passage(CurrentRoom, AdjacentRoom, Keys), 
+            \+ member(move(CurrentRoom, AdjacentRoom), Route),
+            collect_keys(AdjacentRoom, Keys, NewKeys), 
+            append(Route, [move(CurrentRoom, AdjacentRoom)], NewRoute)
+        ),
+        PossibleRoutes
+    ),
+    append(Queue, PossibleRoutes, UpdatedQueue),
+    bfs(UpdatedQueue, FinalRoute). 
+
